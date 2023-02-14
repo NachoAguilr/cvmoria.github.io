49ab8225a8ee70b32e7ee5f49832425f119e2619
NachoAguilr <86528739+NachoAguilr@users.noreply.github.com>
2023-02-14 20:10:57 -0300

    Add files via upload

---
+++ js/cod.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('frase').onclick = function (){
+    console.log("todo bien");
+   document.getElementById('demo').innerHTML = "Soy el Vengador, el Conde de Montecristo con tetas.";
+   
+}
